029720bd244538636c44cd0e3c9e2135fa49b1f1
Sriharish-947 <74801176+Sriharish-947@users.noreply.github.com>
2021-02-20 07:11:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+menuArray = ["veggie pizza", "Normal Pizza", "Normal Chicken Pizza", "Mooshroom Pizza", "Pineapple Pizza", " Normal Pizza but with Pineapple toppings","yum-yum Pizza" ];
+
+ function getMenu()
+ {
+
+    var htmldata;
+    htmldata = "<ol class = 'menulist'>";
+    menuArray.sort();
+    console.log("sorted");
+    for (i=0;i<menuArray.length;i++)
+    {
+        htmldata=htmldata+ '<li>' + menuArray[i] + '</li>'
+       
+    }
+    htmldata=htmldata+ "</ol>"
+    console.log(htmldata);
+    document.getElementById("displayMenu").innerHTML =  htmldata;
+ }
+
+ function  add_item()
+{
+var htmldata
+var item=document.getElementById("add_item").value
+menuArray.push(item);
+menuArray.sort();
+htmldata = '<section class="cards">'
+for (i=0;i<menuArray.length;i++)
+{
+    htmldata= '<div class="cards">' + '<img src="images/pizzaimg.png"/>' + menuArray[i] + '</div>'
+}
+htmldata=htmldata+"</section>"
+document.getElementById("display_addedMenu").innerHTML=htmldata;
+
+}
+
+ 
